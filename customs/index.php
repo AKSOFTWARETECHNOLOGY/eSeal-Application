93c57bcbb71bc45b9a13ff1693e04bca973f3c28
Alagirivimal <alagirivimal@gmail.com>
2017-12-17 02:19:19 +0530

    User Panel Changes Updated

--- customs/index.php
+++ customs/index.php
@@ -17,7 +17,7 @@ include "config.php";
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Admin Panel </title>
+    <title>Customs Panel </title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.4 -->
