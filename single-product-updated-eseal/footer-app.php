dba34e0a5592604aa3cb2d3b9fe2327dfb9a44ec
alagirivimal <alagirivimal@gmail.com>
2018-01-03 23:53:58 +0530

    User Panel Client Comments Updated

---
+++ single-product-updated-eseal/footer-app.php
@@ -0,0 +1,15 @@
+<div class="footer-bottom">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12" style="float: left;">
+            <p style="text-align: left;">SSGA &copy; Copyright <?php echo date('Y'); ?> All Rights Reserved.</p>
+        </div>
+
+        <div class="col-md-6 col-sm-6 col-xs-12" style="float: right;">
+            <p style="text-align: right;">Design & Developed by <a href="http://www.aramsolutions.com" target="_blank">ARAM</a> </p>
+        </div>
+
+
+    </div>
+</div>
+</div>
